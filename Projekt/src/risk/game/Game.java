c78258360485366cede313f19c5ebb67e04cd418
Filip Törnqvist <zoizer123@hotmail.com>
2017-03-02 13:09:34 +0100

    Refactored for local multiplayer
    Signed-off-by: Filip Törnqvist <zoizer123@hotmail.com>

--- Projekt/src/risk/game/Game.java
+++ Projekt/src/risk/game/Game.java
@@ -4,32 +4,23 @@ import risk.event.InputEventManager;
 import risk.event.RiskEvent;
 import risk.event.RiskGameEvent;
 import risk.event.RiskZoneEvent;
-import risk.game.logic.Core;
 import risk.game.logic.GeneralGameEvents;
-import risk.gameview.PlayerGameView;
 import risk.general.event.*;
 import risk.general.util.Delegate;
+import risk.ui.GUI;
+import risk.ui.UI;
 
 public class Game {
-	private PlayerGameView pgv;
-	
-// Test ....
+	@SuppressWarnings("unused")
 	public Game() {
 		EventManager.CreateGlobalEventManager();
 		EventManager inputEventManager = new InputEventManager();
-		pgv = new PlayerGameView(1, inputEventManager);
+
+		UI ui = new UI(inputEventManager);
+		GUI gui = new GUI(ui);
 		
 		AttachListeners();
-		InitGame();
-		/*try {
-			WorldMapInit graphics = new WorldMapInit(); // initierar mappen och knapparna
-		} catch(Exception e) {
-			e.printStackTrace();
-			ErrorHandler.ASSERT(false);
-		}*/
 		
-		Core.Get().AttachGameView(pgv);
-
 		while(true) {
 			inputEventManager.Update();
 			EventManager.Get().Update();
@@ -51,10 +42,6 @@ public class Game {
 		EventManager.Get().AttachListener(new Delegate(this, "RequestNewGame"), RiskEvent.EVENT_NEW_GAME_POST);
 		EventManager.Get().AttachListener(new Delegate(GeneralGameEvents.Get(), "LoadMap"), RiskGameEvent.EVENT_NEW_GAME);
 		EventManager.Get().AttachListener(new Delegate(GeneralGameEvents.Get(), "SelectZone"), RiskZoneEvent.EVENT_SELECT_ZONE);
-	}
-
-
-	private void InitGame() {
 	}
 	
 	public void RequestNewGame(IEvent event) {
